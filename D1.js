728c9612eeec2cf75734f18e398a3f8b7476d0c0
alessiacamp <alessiacampanile14@gmail.com>
2024-10-07 16:05:08 +0200

    esercizio terminato

--- D1.js
+++ D1.js
@@ -11,38 +11,54 @@ REGOLE
  Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let nome="mario";
+/* a (nome della variabile), = (assegnazione), 2 (valore stringa).*/
+let booleano=true;
+// booleano=(nome della variabile), = (assegnazione), valore booleano che può essere true o false.
+let numero=2;
+/* a (nome della variabile), = (assegnazione), 2 (valore numerico).*/
 
 /* ESERCIZIO 2
  Crea una variable chiamata "myName" e assegna ad essa il tuo nome, sotto forma di stringa.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let myName = "Alessia";
+console.log(myName);
 
 /* ESERCIZIO 3
  Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let a=12;
+let b=20;
+let somma=a+b;
+console.log(somma);
 
 /* ESERCIZIO 4
  Crea una variable di nome "x" e assegna ad essa il numero 12.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let x=12;
 
 /* ESERCIZIO 5
   Riassegna un nuovo valore alla variabile "myName" già esistente: il tuo cognome.
   Dimostra l'impossibilità di riassegnare un valore ad una variabile dichiarata con il costrutto const.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+myName="Campanile";
+console.log(myName);
+const surname="Bianchi";
+//surname="Rossi"; ERRORE
+console.log(surname);
 
 /* ESERCIZIO 6
  Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let sottrazione=x-4;
+console.log(sottrazione);
+
+
 
 /* ESERCIZIO 7
  Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
@@ -50,4 +66,10 @@ REGOLE
  EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let name1="john";
+let name2="John";
+
+let condizione=name1===name2;
+console.log(condizione);
+condizione=name1.toLowerCase()===name2.toLowerCase();
+console.log(condizione);
